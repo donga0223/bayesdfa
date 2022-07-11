1093207957e3b7351005fd5e6fa9ce27aa05c52f
Dongah Kim <donga0223@gmail.com>
2022-07-11 17:24:31 -0400

    modified

--- R/sim.R
+++ R/sim.R
@@ -105,6 +105,70 @@
 #' matplot(t(x$pred), type = "l")
 #' abline(v = 10)
 #' @export
+sim_dfa_DA <- function (num_trends = 1, num_years = 20, num_ts = 4, phi = c(0.8)
+                        , loadings_matrix = matrix(nrow = num_ts, ncol = num_trends, rnorm(num_ts * num_trends, 0, 1))
+                        , sigma = rlnorm(num_years,meanlog = log(0.2), 0.1)
+                        , varIndx = rep(1, num_ts), extreme_value = NULL
+                        , extreme_loc = NULL, nu_fixed = 100, user_supplied_deviations = NULL) 
+{
+  y_ignore <- matrix(rnorm(num_ts * num_years), nrow = num_ts, 
+                     ncol = num_years)
+  d <- fit_dfa(y_ignore, num_trends = num_trends, estimation = "none", 
+               scale = "center", varIndx = varIndx, nu_fixed = nu_fixed, 
+               trend_model = "rw")
+  Z <- loadings_matrix
+  y <- vector(mode = "numeric", length = d$sampling_args$data$N)
+  for (k in seq_len(d$sampling_args$data$K)) {
+    Z[k, k] <- abs(Z[k, k])
+  }
+  for (k in seq_len(d$sampling_args$data$K)) {
+    for (p in seq_len(d$sampling_args$data$P)) {
+      if (p < k) 
+        Z[p, k] <- 0
+    }
+  }
+  x <- matrix(nrow = d$sampling_args$data$K, ncol = d$sampling_args$data$N)
+  for (k in seq_len(d$sampling_args$data$K)) {
+    if (!is.null(user_supplied_deviations)) {
+      devs <- user_supplied_deviations[, k]
+    }
+    else {
+      devs <- rt(d$sampling_args$data$N, df = d$sampling_args$data$nu_fixed)
+    }
+    x[k, 1] <- rnorm(1, 0, 1)
+    if (is.null(extreme_value)) {
+      for (t in seq(2, d$sampling_args$data$N)) {
+        x[k, t] <- phi[k]*x[k, t - 1] + devs[t]
+      }
+    }
+    else {
+      if (is.null(extreme_loc)) 
+        extreme_loc <- round(num_years/2)
+      for (t in 2:(extreme_loc - 1)) {
+        x[k, t] <- phi[k]*x[k, t - 1] + devs[t]
+      }
+      if (k == 1) {
+        x[1, extreme_loc] <- phi[k]*x[1, extreme_loc - 1] + 
+          extreme_value
+      }
+      else {
+        x[k, extreme_loc] <- phi[k]*x[k, extreme_loc - 1] + 
+          devs[t]
+      }
+      for (t in seq(extreme_loc + 1, d$sampling_args$data$N)) {
+        x[k, t] <-phi[k]* x[k, t - 1] + devs[t]
+      }
+    }
+  }
+  pred <- Z %*% x
+  for (i in seq_len(d$sampling_args$data$n_pos)) {
+    y[i] <- rnorm(1, pred[d$sampling_args$data$row_indx_pos[i], 
+                          d$sampling_args$data$col_indx_pos[i]], sigma[d$sampling_args$data$col_indx_pos[i]])
+  }
+  y_sim <- matrix(y, nrow = d$sampling_args$data$P)
+  list(y_sim = y_sim, pred = pred, x = x, Z = Z, sigma = sigma, phi = phi)
+}
+
 
 sim_dfa <- function(num_trends = 1,
                     num_years = 20,
@@ -180,67 +244,4 @@ sim_dfa <- function(num_trends = 1,
   }
   y_sim <- matrix(y, nrow = d$sampling_args$data$P)
   list(y_sim = y_sim, pred = pred, x = x, Z = Z, sigma = sigma)
-}
-sim_dfa_DA <- function (num_trends = 1, num_years = 20, num_ts = 4, phi = c(0.8)
-                        , loadings_matrix = matrix(nrow = num_ts, ncol = num_trends, rnorm(num_ts * num_trends, 0, 1))
-                        , sigma = rlnorm(num_years,meanlog = log(0.2), 0.1)
-                        , varIndx = rep(1, num_ts), extreme_value = NULL
-                        , extreme_loc = NULL, nu_fixed = 100, user_supplied_deviations = NULL) 
-{
-  y_ignore <- matrix(rnorm(num_ts * num_years), nrow = num_ts, 
-                     ncol = num_years)
-  d <- fit_dfa(y_ignore, num_trends = num_trends, estimation = "none", 
-               scale = "center", varIndx = varIndx, nu_fixed = nu_fixed, 
-               trend_model = "rw")
-  Z <- loadings_matrix
-  y <- vector(mode = "numeric", length = d$sampling_args$data$N)
-  for (k in seq_len(d$sampling_args$data$K)) {
-    Z[k, k] <- abs(Z[k, k])
-  }
-  for (k in seq_len(d$sampling_args$data$K)) {
-    for (p in seq_len(d$sampling_args$data$P)) {
-      if (p < k) 
-        Z[p, k] <- 0
-    }
-  }
-  x <- matrix(nrow = d$sampling_args$data$K, ncol = d$sampling_args$data$N)
-  for (k in seq_len(d$sampling_args$data$K)) {
-    if (!is.null(user_supplied_deviations)) {
-      devs <- user_supplied_deviations[, k]
-    }
-    else {
-      devs <- rt(d$sampling_args$data$N, df = d$sampling_args$data$nu_fixed)
-    }
-    x[k, 1] <- rnorm(1, 0, 1)
-    if (is.null(extreme_value)) {
-      for (t in seq(2, d$sampling_args$data$N)) {
-        x[k, t] <- phi[k]*x[k, t - 1] + devs[t]
-      }
-    }
-    else {
-      if (is.null(extreme_loc)) 
-        extreme_loc <- round(num_years/2)
-      for (t in 2:(extreme_loc - 1)) {
-        x[k, t] <- phi[k]*x[k, t - 1] + devs[t]
-      }
-      if (k == 1) {
-        x[1, extreme_loc] <- phi[k]*x[1, extreme_loc - 1] + 
-          extreme_value
-      }
-      else {
-        x[k, extreme_loc] <- phi[k]*x[k, extreme_loc - 1] + 
-          devs[t]
-      }
-      for (t in seq(extreme_loc + 1, d$sampling_args$data$N)) {
-        x[k, t] <-phi[k]* x[k, t - 1] + devs[t]
-      }
-    }
-  }
-  pred <- Z %*% x
-  for (i in seq_len(d$sampling_args$data$n_pos)) {
-    y[i] <- rnorm(1, pred[d$sampling_args$data$row_indx_pos[i], 
-                          d$sampling_args$data$col_indx_pos[i]], sigma[d$sampling_args$data$col_indx_pos[i]])
-  }
-  y_sim <- matrix(y, nrow = d$sampling_args$data$P)
-  list(y_sim = y_sim, pred = pred, x = x, Z = Z, sigma = sigma, phi = phi)
 }
